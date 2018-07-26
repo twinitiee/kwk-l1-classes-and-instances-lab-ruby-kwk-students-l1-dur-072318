ae0f8cca14b0eff75328a2cf6c2e2e17cae67fd2
twinitiee <twinitiee@gmail.com>
2018-07-26 20:08:21 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,13 @@
 # dog.rb
+
+class Dog
+  def initialize(name)
+    @name=name
+  end
+  
+end
+
+fido=Dog.new("fido")
+snoopy=Dog.new("snoopy")
+lassie=Dog.new("lassie")
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,9 @@
 # person.rb
+class Person
+  def initialize(name)
+  @name=name
+  end
+end 
+  adele_goldberg=Person.new("adele_goldberg")
+  alan_kay=Person.new("alan_kay")
+  
